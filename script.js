280f753ec154d5f9a3ff75665eb8a75023a97081
shravan kumar <117439152+borrolla@users.noreply.github.com>
2025-03-22 20:24:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,42 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  employees.map((employee) => {
+    if (employee.profession === "developer") {
+      console.log(employee);
+    }
+  });
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+   employees.forEach((employee) => {
+    if (employee.profession === "developer") {
+      console.log(employee);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  const newEmployee = { id: 4, name: "susan", age: "20", profession: "intern" };
+  employees.push(newEmployee);
+  console.log(employees);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  const filteredEmployees = employees.filter(employee => employee.profession !== "admin");
+  console.log(filteredEmployees);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const newEmployees = [
+    { id: 5, name: "alice", age: "22", profession: "manager" },
+    { id: 6, name: "bob", age: "23", profession: "developer" },
+    { id: 7, name: "eve", age: "21", profession: "designer" }
+  ];
+  const combinedArray = employees.concat(newEmployees);
+  console.log(combinedArray)
 }
