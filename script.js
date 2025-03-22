8ab953830936e8e2363dc2b6c97e3c2ae9290f10
Jonnada Sai Teja <146433770+saitejajonnada@users.noreply.github.com>
2025-03-22 17:27:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,21 +7,40 @@ let arr = [
 ];
 
 function PrintDeveloperbyMap() {
-  //Write your code here , just console.log
+  arr.map(emp=>{
+    if(emp.profession === "developer")
+    {
+      console.log(emp);
+    }
+  });
 }
 
 function PrintDeveloperbyForEach() {
-  //Write your code here , just console.log
+  arr.forEach(emp=>{
+    if(emp.profession === "developer"){
+      console.log(emp);
+    }
+  });
 }
 
 function addData() {
-  //Write your code here, just console.log
+   let newEmployee = { id: 4, name: "susan", age: "20", profession: "intern" };
+    employees.push(newEmployee);
+    console.log(employees);
 }
 
 function removeAdmin() {
-  //Write your code here, just console.log
+  employees = employees.filter(emp => emp.profession !== "admin");
+    console.log(employees);
 }
 
 function concatenateArray() {
-  //Write your code here, just console.log
+let newEmployees = [
+        { id: 5, name: "alice", age: "22", profession: "designer" },
+        { id: 6, name: "bob", age: "25", profession: "manager" },
+        { id: 7, name: "charlie", age: "21", profession: "developer" }
+    ];
+    
+    let combinedArray = employees.concat(newEmployees);
+    console.log(combinedArray);
 }
